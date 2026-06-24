56c21d920f164b47f930aef666d774200968ba90
Michael Charles Aubrey <aubrey@michaelcharl.es>
2026-06-24 14:38:15 +0900

    Initial Commit

---
+++ PokéJisho/Poke_JishoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_JishoApp.swift
+//  PokéJisho
+//
+//  Created by Michael Charles Aubrey on 2026/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Poke_JishoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
